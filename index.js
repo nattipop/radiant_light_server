7602d4a0e594190ac937bcadf4548f960aa845f4
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-01-06 09:18:00 -0600

    photo model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const cors = require("cors");
+const { Photo, Event } = require("/models");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -15,6 +16,25 @@ db.once("open", () => console.log("connected to database"))
 
 app.get("/", cors(), async (req, res) => {
   res.send("this is working")
+})
+
+app.post("/new-photo", cors(), async (req, res) => {
+  const data = req.body;
+  
+  if(!data.title) res.status(422).send("Photo title required");
+  if(!data.photo_id) res.status(422).send("Photo id required");
+  if(!data.url) res.status(422).send("Photo url required");
+  if(!data.category) res.status(422).send("Photo category required");
+
+  const photo = new Photo({
+    title: data.title,
+    description: data.description,
+    photo_id: data.photo_id,
+    url: data.url,
+    category: data.category
+  });
+  photo.save();
+  res.status(200).send("Successfully added photo")
 })
 
 app.listen(port, () => {
